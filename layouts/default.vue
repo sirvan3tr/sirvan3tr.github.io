2d24dcfb1ae2bb6ab1fbf668e74996569d199ee0
Sirvan Almasi <sirvan3tr@gmail.com>
2021-11-26 10:46:06 +0000

    first version

---
+++ layouts/default.vue
@@ -0,0 +1,62 @@
+<template lang="pug">
+v-app(dark='')
+  // v-navigation-drawer(v-model='drawer' :mini-variant='miniVariant' :clipped='clipped' fixed app)
+  //   v-list
+  //     v-list-item(v-for='(item, i) in items' :key='i' :to='item.to' router exact)
+  //       v-list-item-action
+  //         v-icon {{ item.icon }}
+  //       v-list-item-content
+  //         v-list-item-title(v-text='item.title')
+  // v-app-bar(:clipped-left='clipped' fixed app)
+  //   v-app-bar-nav-icon(@click.stop='drawer = !drawer')
+  //   v-btn(icon='' @click.stop='miniVariant = !miniVariant')
+  //     v-icon mdi-{{ &grave;chevron-${miniVariant ? &apos;right&apos; : &apos;left&apos;}&grave; }}
+  //   v-btn(icon='' @click.stop='clipped = !clipped')
+  //     v-icon mdi-application
+  //   v-btn(icon='' @click.stop='fixed = !fixed')
+  //     v-icon mdi-minus
+  //   v-toolbar-title(v-text='title')
+  //     v-spacer
+  //       v-btn(icon='' @click.stop='rightDrawer = !rightDrawer')
+  //         v-icon mdi-menu
+  v-main
+    v-container
+      nuxt
+  v-navigation-drawer(v-model='rightDrawer' :right='right' temporary='' fixed='')
+    v-list
+      v-list-item(@click.native='right = !right')
+        v-list-item-action
+          v-icon(light='')
+            | mdi-repeat
+        v-list-item-title Switch drawer (click me)
+  v-footer(:absolute='!fixed' app='')
+    span &copy; {{ new Date().getFullYear() }}
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Sirvan Almasi'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,84 @@
+<template lang="pug">
+v-container
+  p.text-h3 Sirvan Almasi
+  p Welcome to my personal page, I am a PhD Candidate at Imperial College London's Computing department, and under the supervision of Prof William Knottenbelt and Prof Philip Treleaven.
+  p
+    v-btn(text href='https://github.com/sirvan3tr')
+      v-icon(small left) mdi-github
+      | Github
+    v-btn(text href='https://www.linkedin.com/in/salmasi/')
+      v-icon(small left) mdi-linkedin
+      | LinkedIn
+    v-btn(text href='https://twitter.com/sevthreetr')
+      v-icon(small left) mdi-twitter
+      | Twitter
+  v-row.d-flex
+    v-col(lg='9')
+      v-card
+        v-card-title Projects
+        v-card-text
+          ol
+            li
+              a(href='http://innovationlab.uk') InnovationLab.UK
+              v-icon(small) mdi-launch
+              | Rethinking the science of innovation
+            li
+              a(href='http://innovoteams.com') InnovoTeams
+              v-icon(small) mdi-launch
+              | Applying NLP techniques in connecting creators using skills and personality.
+            li
+              strong The Innovation Estimate (draft one coming soon)
+              | Analytical framework to create a strategy for a venture. Based on The Combat Estimate.
+            li
+              | deeID: Identity and Authentication System. Now part of 
+              a(href='https://conceptionx.org/' target='_blank') ConceptionX&apos;s
+              |  Cohort III venture builder.
+            li Document Provenance Using Blockchain
+            li
+              strong
+                a(href='http://www.smartcommuter.innovationlab.uk/' target='_blank') SmartCommuter
+                v-icon(small) mdi-launch
+              |  Novel property search engine.
+            li
+              a(href='docs/essays/royalsignalessay_sirvan.pdf') Impact of technology on the battlefield of the future
+              |  [pdf]
+      v-card.mt-3
+        v-card-title Papers and publications
+        v-card-text
+          ul
+            li
+              | S. Almasi and W.J. Knottenbelt. 
+              a(href='docs/papers/deeID_USENIX_Feb_2020wnames.pdf') Human Identity and the Quest to Replace Password Continued
+              |  - Being Reviewed
+            li
+              | S. Almasi and W.J. Knottenbelt. 
+              a(href='docs/papers/Protecting_user_secrets_NDSS_Workshop.pdf') Protecting Users from Compromised Browsers andForm Grabbers.
+              i NDSS Workshop on Measurements, Attacks, and Defenses for the Web (MADWeb 2020)
+              | , San Diego, California, USA, Feb 2020.
+      v-card.mt-3
+        v-card-title Presentations
+        v-card-text
+          ul
+            li
+              | Innovation of Bitcoin--how to take it mainstream. 
+              a(href='docs/presentations/2020-10-09_sirvan_bitcoin-innovation') [pdf]
+    v-col(lg='3')
+      v-card
+        v-card-title Links and other
+        v-card-text
+          ul
+            li
+              a(href='http://www.norvig.com/') Peter Norvig
+              v-icon(small) mdi-launch
+            li
+              a(href='http://norvig.com/21-days.html') Teach yourself programming in ten years
+              v-icon(small) mdi-launch
+            li
+              a(href='http://www.catb.org/~esr/faqs/hacker-howto.html') Eric Raymond&apos;s How to become a hacker
+              v-icon(small) mdi-launch
+            li
+              a(href='http://www.paulgraham.com/useful.html') Paul Graham&apos;s How to write usefully
+              v-icon(small) mdi-launch
+            li
+              a(href='/static/docs/inspire/Toolsmith-CACM.pdf') The Computer Scientist as a Toolsmith [pdf]
+</template>
