a5d209cbd97e9b65927d1f55a5f893a4cddf6acf
Sirvan Almasi <sirvan3tr@gmail.com>
2024-04-24 23:22:08 +0000

    website update

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
 v-container
   p.text-h3 Sirvan Almasi
   p Welcome to my personal page, I am a PhD Candidate at Imperial College London's Computing department, and under the supervision of Prof William Knottenbelt and Prof Philip Treleaven.
+  p Please note that this website does not include everything about me and my work.
   p
     v-btn(text href='https://github.com/sirvan3tr')
       v-icon(small left) mdi-github
@@ -12,49 +13,111 @@ v-container
     v-btn(text href='https://twitter.com/sevthreetr')
       v-icon(small left) mdi-twitter
       | Twitter
+
   v-row.d-flex
     v-col(lg='9')
       v-card
         v-card-title Projects
         v-card-text
+          div.text-caption Some of the projects I have worked on or am working on in no particular order.
           ol
             li
-              a(href='http://innovationlab.uk') InnovationLab.UK
-              v-icon(small) mdi-launch
-              | Rethinking the science of innovation.
+              a(href="https://sirvan3tr.github.io/pandas_game/")
+                h3 Pandas Game
+              div.text-caption Prototype of a game to learn coding, e.g. Pandas.
             li
-              a(href='http://innovoteams.com') InnovoTeams
-              v-icon(small) mdi-launch
-              | Applying NLP techniques in connecting creators using skills and personality.
+              a(href="https://aquamarine-cocada-c77c38.netlify.app")
+                h3 ICLR2024 Explorer
+              div.text-caption Simple app to filter and explore ICLR2024 papers.
             li
-              strong The Innovation Estimate (draft one coming soon)
-              | Analytical framework to create a strategy for a venture. Based on The Combat Estimate.
+              h3 AXON
+              div.text-caption Re-make of the OpenBCI EEG hardware using ESP32.
             li
-              | deeID: Identity and Authentication System. Now part of 
-              a(href='https://conceptionx.org/' target='_blank') ConceptionX&apos;s
-              |  Cohort III venture builder.
-            li Document Provenance Using Blockchain.
+              h3 Stoic Writer
+              div.text-caption Writing app that forces you to just write. It also tracks your progress.
             li
-              strong
-                a(href='http://www.smartcommuter.innovationlab.uk/' target='_blank') SmartCommuter
-                v-icon(small) mdi-launch
-              |  Novel property search engine.
+              h3 InnovationLab (at UCL with Prof. Philip Treleaven)
+              div.text-caption Rethinking the science of innovation.
             li
-              a(href='docs/essays/royalsignalessay_sirvan.pdf') Impact of technology on the battlefield of the future.
-              |  [pdf]
+              h3 InnovoTeams
+              div.text-caption Creating high-performing teams and co-founder matching using skills (via NLP techniques) and persoanlity matching.
+            li
+              h3 The Innovation Estimate.
+              div.text-caption Analytical framework to create a strategy for a venture. Based on The Combat Estimate.
+            li
+              h3 deeID: Identity and Authentication System.
+            li 
+              h3 Document Provenance Using Blockchain.
+            li
+              h3 SmartCommuter (Founder)
+              div.text-caption
+                | Novel property search engine.
+                |  Was part of 
+                a(href='https://conceptionx.org/' target='_blank') ConceptionX&apos;s
+                |  Cohort III accelerator programme.
+            li
+              h3 BrainGym (Engineer and Co-founder)
+              div.text-caption
+                | Neurofeedback device to improve focus and concentration.
+                |  Was part of 
+                a(href='https://conceptionx.org/' target='_blank') ConceptionX&apos;s
+                |  Cohort 4 accelerator programme.
+
       v-card.mt-3
-        v-card-title Papers and publications
+        v-card-title Posts
         v-card-text
           ul
             li
-              | S. Almasi and W.J. Knottenbelt. 
-              a(href='docs/papers/deeID_USENIX_Feb_2020wnames.pdf') Human Identity and the Quest to Replace Password Continued
-              |  - Being Reviewed
+              a(href="docs/gpt2.pdf")
+                h4 Mathematics of the Attention Layer (draft)
+              div.text-caption My own mathematical intuition of the attention layer.
+      v-card.mt-3
+        v-card-title Papers, Essays, and Dissertations
+        v-card-text
+          ol
             li
-              | S. Almasi and W.J. Knottenbelt. 
+              | Mar 2024 - S. Almasi and W.J. Knottenbelt.  
+              a(href="docs/20240319_passwordninja.pdf") PasswordNinja: Password Composition and Complexity Insights from Manually Labelled Datasets
+            li
+              | Mar 2024 - S.Almasi.  
+              a(href="docs/papers/20240422_tie.pdf") The Innovation Estimate
+            li
+              | Mar 2022 - S. Almasi and W.J. Knottenbelt.  
+              a(href='docs/papers/api_modelling_techniques.pdf') API Modelling Techniques 
+            li
+              | Feb 2020 - S. Almasi and W.J. Knottenbelt. 
               a(href='docs/papers/Protecting_user_secrets_NDSS_Workshop.pdf') Protecting Users from Compromised Browsers andForm Grabbers.
-              i NDSS Workshop on Measurements, Attacks, and Defenses for the Web (MADWeb 2020)
+              i  NDSS Workshop on Measurements, Attacks, and Defenses for the Web (MADWeb 2020)
               | , San Diego, California, USA, Feb 2020.
+            li
+              | Oct 2019 - S. Almasi  
+              a(href='docs/essays/royalsignalessay_sirvan.pdf') Impact of Technology on the Battlefield of the Future.
+            li
+              | Jan 2019 - S. Almasi and W.J. Knottenbelt.  
+              a(href='docs/papers/deeID_USENIX_Feb_2020wnames.pdf') Human Identity and the Quest to Replace Password Continued
+          hr
+          v-row
+            v-col(cols="auto")
+              img(src="img/lsr_poster_small.png")
+            v-col
+              div.text-caption PhD Computer Science - 2024
+              h3 Protecting User Secrets in Hostile Environments (ongoing)
+              a(href="docs/20240404-LSR_poster-almasi.pdf")
+                v-icon(color="red") mdi-file-pdf-box
+                | Late Stage Review Poster
+          hr
+          v-row
+            v-col(cols="auto")
+              img(src="img/beng-poster-small.jpg")
+            v-col
+              div.text-caption BEng Aerospace Engineering Dissertation - 2014
+              h3 Use of Turboprop Propelled Aircraft and Cargo UAV’s for Long Haul Flights
+              a(href="img/beng-poster.jpg")
+                v-icon(color="red") mdi-file-image
+                | POSTER
+              a(href="docs/degrees/Use of turboprop propelled aircraft and Cargo UAV.pdf") 
+                v-icon(color="red") mdi-file-pdf-box
+                | DISSERTATION
       v-card.mt-3
         v-card-title Presentations
         v-card-text
